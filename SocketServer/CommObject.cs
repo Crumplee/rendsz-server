dbeb7f9ed2f6824e5ff1ed36b47682d15ccd392c
Crumplee <raba.bence95@gmail.com>
2018-04-11 20:45:56 +0200

    poc

---
+++ SocketServer/CommObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Communication
+{
+    [Serializable]
+    public class CommObject
+    {
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+
+        public CommObject() { }
+        public CommObject(string msg)
+        {
+            this.Message = msg;
+            this.Date = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return Message;
+        }
+    }
+}
---
+++ SocketServer/ServerService.cs
@@ -0,0 +1,147 @@
+﻿using Communication;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace SocketServer
+{
+    public class AsyncService
+    {
+        private IPAddress ipAddress;
+        private int port;
+        public AsyncService(int port)
+        {
+            this.port = port;
+            
+            this.ipAddress = IPAddress.Loopback;
+            if (this.ipAddress == null)
+                throw new Exception("No IPv4 address for server");
+        }
+        public async void Run()
+        {
+            TcpListener listener = new TcpListener(this.ipAddress, this.port);
+            listener.Start();
+            Console.Write("Test socket service is now running");
+            Console.WriteLine(" " + this.ipAddress + " on port " + this.port);
+            Console.WriteLine("Hit <enter> to stop service\n");
+            while (true)
+            {
+                try
+                {
+                    TcpClient tcpClient = await listener.AcceptTcpClientAsync();
+                    Task t = Process(tcpClient);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        private async Task Process(TcpClient tcpClient)
+        {
+            string clientEndPoint = tcpClient.Client.RemoteEndPoint.ToString();
+            Console.WriteLine("Received connection request from " + clientEndPoint);
+            try
+            {
+                NetworkStream networkStream = tcpClient.GetStream();
+                StreamReader reader = new StreamReader(networkStream);
+                StreamWriter writer = new StreamWriter(networkStream);
+                writer.AutoFlush = true;
+                
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+                while (true)
+                {
+                    string requestStr = await reader.ReadLineAsync();
+                    if (requestStr != null)
+                    {
+                        CommObject request = serializer.Deserialize<CommObject>(requestStr);
+                        Console.WriteLine("Received service request: " + request);
+                        
+
+                        CommObject response = Response(request);
+                        Console.WriteLine("Computed response is: " + response + "\n");
+                        await writer.WriteLineAsync(serializer.Serialize(response));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Connection closed, client: " + clientEndPoint);
+                        break; // Client closed connection
+                    }
+                }
+                tcpClient.Close();
+            }
+            catch (Exception ex)
+            {                
+                if (tcpClient.Connected)
+                {                    
+                    tcpClient.Close();
+                }
+                Console.WriteLine("Connection closed, client: " + clientEndPoint);
+            }
+        }
+        private static CommObject Response(CommObject request)
+        {
+            string reqdata = request.Message;
+            char cmd = reqdata[0];
+
+            string data = reqdata.Substring(1, reqdata.Length - 1);
+            string response_str = "";
+            switch (cmd)
+            {
+                case '1':
+                    response_str = DateTime.Now.ToString();
+                    break;
+                case '2':
+                    response_str = Reverse(data);
+                    break;
+                case '3':
+                    response_str = ToUpPeR(data);
+                    break;
+                default:
+                    response_str = cmd + data;
+                    break;
+            }
+            CommObject response = new CommObject(response_str + " --(Server)--");
+            return response;          
+           
+        }
+
+        private static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        private static string ToUpPeR(string s)
+        {
+            string res = "";
+            for(int i = 0; i < s.Length; ++i)
+            {
+                if (i % 2 == 0)
+                    res += char.ToUpper(s[i]);
+                else
+                    res += s[i];
+            }
+          
+            res += "\n" + Mocking();
+            return res;
+        }
+
+        private static string Mocking()
+        {
+            string[] lines = File.ReadAllLines("mocking.txt");
+            string m = "";
+            foreach (string line in lines)
+            {
+                m += line + "\n";
+            }
+            return m;
+        }
+
+    }
+}
